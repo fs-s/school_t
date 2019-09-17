2654c9d908e005c4bbc6ec4ffb2ef759aa7f4d1e
fs-s <55434294+fs-s@users.noreply.github.com>
2019-09-17 11:40:19 +0300

    prog
    k

---
+++ prog/6.3 t.py
@@ -0,0 +1,2 @@
+n = input("sisetage täisarv")
+print (n + "+" + n + n + "=" + str(int(n) + int(n + n)))  
---
+++ prog/6.3.py
@@ -0,0 +1,7 @@
+n = int(input("sisestage täisarv"))
+k=str(n)
+t1=k+k
+t2=k+k+k
+comp=n+int(t1)+int(t2)
+comp=n+int(t1)+int(t2)
+print("vastus",comp)
