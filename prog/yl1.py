a421b0aef14d4c513c5d77e31d301151cd49b43f
fs-s <55434294+fs-s@users.noreply.github.com>
2019-10-25 11:39:15 +0300

    Add files via upload

--- prog/yl1.py
+++ prog/yl1.py
@@ -1,9 +1,23 @@
-a=int(input("a: "))
-b=int(input("b: "))
-c=int(input("c: "))
-if a > b and a > c:  
-    print("a on suurem, kui b ja c")
-if b > a and b > c:
-    print("b on suurem, kui a ja c ")
-elif b > c:
-    print("c on suurem kui a ja b " )
+#vaja kirjutada list ja lisada 3 puuvilja, väljastada esimene väärtus
+thislist = ["õun","pirn","kirss"]
+print(thislist[0])
+#lisada uus puuvili
+thislist.append("ploom")
+print (thislist[3])
+thislist = ["õun","pirn","kirss","banaan"]
+thislist[1] = "kiivi"
+print(thislist)
+thislist = ["õun","pirn","kirss","ploom"]
+if "õun" in thislist:
+    print("jah, 'õun' on puuvilja listis olemas")
+thislist = ["õun","pirn","kirss","kiivi","banaan"]
+print(len(thislist))
+thislist = ["õun","pirn","kirss","kiivi","banaan"]
+thislist.remove("banaan")
+print(thislist)
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+thislist.reverse()
+print(thislist)
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+thislist.sort()
+print(thislist)
