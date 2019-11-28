7594edf3fc7e6d414e8d813d598efca9a136c84f
fs-s <55434294+fs-s@users.noreply.github.com>
2019-11-28 10:02:10 +0200

    hangman

---
+++ yy.py
@@ -0,0 +1,12 @@
+word="ametikool"
+used_word=["_,""_,""_,""_,""_,""_,""_,""_,""_,"]
+used_letters= []
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+letters = list(word.lower())
+
+while True:
+    print (used_wordg)
+    print ("kasutatud tähed"+ str(used_letters))
+    letter=input("sisestage üks täht :")
+    used_letters.append(letter)    
