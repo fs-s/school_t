e7b5681bd8867c171b677cc3b31a7f6f8c47c9fa
fs-s <55434294+fs-s@users.noreply.github.com>
2019-11-29 09:52:52 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,6 @@
+print("Pärast purjus öö koos sõpradega, sa ärkad üles"
+"järgmisel hommikul ärkad üles pimedas metas. Pea käib ringin ja"
+"võideldes tunde vastu oksendada, seisad ja imestad oma uuete riiete üle,"
+" Rahu hääbub kiiresti, kui kuulete"
+" selja tagant Karjuv heli. Roheline orc on"
+" jooksed tema poole. mida sa teed:")
